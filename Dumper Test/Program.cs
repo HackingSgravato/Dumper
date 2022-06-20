d5f91b12bfb8ab972e7defda0db07fe1e2ffd9d7
HackingSgravato <94282136+HackingSgravato@users.noreply.github.com>
2022-06-20 18:15:00 +0200

    Add files via upload

---
+++ Dumper Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using DumperN;
+
+namespace Dumper_Test
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Dumper d = new Dumper();
+            d.addProcess("explorer");
+            d.addProcess("6100");
+            d.addProcess("3616");
+            d.customLoad(); 
+
+            Console.WriteLine("processes dumped in " + d.ElapsedMilliseconds + "ms");
+            Console.WriteLine(d.warningMessage);
+            foreach (string resultFile in d.resultFiles)
+                Process.Start(resultFile);
+
+            Console.ReadKey();
+        }
+    }
+}
